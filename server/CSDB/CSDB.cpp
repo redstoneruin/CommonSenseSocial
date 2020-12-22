1891ee82b4977ca3e94d207f326eda56f1c8cd63
Ryan Steinwert <rsteinwe@ucsc.edu>
2020-12-22 15:56:26 -0800

    Outline for recursive collection resolution from file

--- server/CSDB/CSDB.cpp
+++ server/CSDB/CSDB.cpp
@@ -8,10 +8,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <string>
 
 #include <sys/stat.h>
 #include <sys/types.h>
+
+#include "../definitions.h"
 
 #include "CSDB.h"
 
@@ -27,9 +30,11 @@ CSDB::CSDB() : CSDB("db")
  * Create db with the given filename
  * @dirname the directory containing the database
  */
-CSDB::CSDB(const char* dirname)
+CSDB::CSDB(const char* dirname) :
+    _currentCollDepth(0),
+    _collIndex(0),
+    _currentParent(nullptr)
 {
-    
     _dbDirname = dirname;
 
     setup();
@@ -53,11 +58,99 @@ void CSDB::setup()
     std::string collFilename(_dbDirname);
     collFilename.append("/collections");
 
-    // create collection file if dne
-    int collfd = open(collFilename.c_str(), O_RDWR | O_CREAT);
-    if(collfd == -1) {
+
+    loadDB(collFilename.c_str());
+}
+
+
+/**
+ * Load DB using collections file
+ * @collsFilename Filename for collections
+ */
+void CSDB::loadDB(const char* collsFilename)
+{
+    int fd;
+    FILE* file;
+    char collNameBuf[MAX_COLLECTION_NAME_SIZE + 1];
+
+    fd = open(collsFilename, O_RDWR | O_CREAT);
+    
+    if(fd == -1) {
         fprintf(stderr, "Could not open collections file\n");
+        return;
     }
 
-    close(collfd);
+    // open file in read only mode
+    file = fdopen(fd, "r");
+    if(file == nullptr) {
+        fprintf(stderr, "Could not open collections file stream\n");
+        return;
+    }
+
+    // read through file parsing by whitespace
+    while(fscanf(file, "%s", collNameBuf) == 1) {
+        // parse the collection using helper function
+        parseCollectionString(collNameBuf);
+    }
+
+    close(fd);
+}
+
+
+/**
+ * Parse a collection string
+ * @collectionString C string containing name and number of subcollections to parse
+ */
+void CSDB::parseCollectionString(char* collectionString)
+{
+    // find ':' to parse the number of subcollections
+    int colPos, nameLen;
+    collection_s* newColl = (collection_s*) malloc (sizeof(collection_s));
+    newColl->parent = nullptr;
+    newColl->path = nullptr;
+    newColl->subCollections = nullptr;
+    newColl->items = nullptr;
+    
+    nameLen = strlen(collectionString);
+    for(int i = 0; i < nameLen; i++) {
+        if(collectionString[i] == ':') {
+            colPos = i;
+            collectionString[i] = (char)0;
+        }
+    }
+
+    char* numSubCollsString = collectionString + colPos + 1;
+
+    // fill collection struct
+    newColl->numSubColls = atoi(numSubCollsString);
+    newColl->name = (char*) malloc (sizeof(char) * (colPos + 1));
+    strncpy(newColl->name, collectionString, colPos + 1);
+
+    // set path based on parent
+    if(_currentParent == nullptr) {
+        newColl->parent = nullptr;
+        newColl->path = (char*) malloc (sizeof(char) * (colPos + 1));
+        strncpy(newColl->path, collectionString, colPos + 1);
+    } else {
+        newColl->parent = _currentParent;
+        
+        // create path string
+        std::string pathstring(_currentParent->path);
+        pathstring.append("/");
+        pathstring.append(collectionString);
+
+        newColl->path = (char*) malloc (sizeof(char) * (pathstring.length() + 1));
+        strncpy(newColl->path, pathstring.c_str(), pathstring.length() + 1);
+
+    }
+
+    printf("---Added collection---\n");
+    printf("Name: %s\n", newColl->name);
+    printf("Path: %s\n", newColl->path);
+    printf("Num subcollections: %d\n", newColl->numSubColls);
+    printf("Parent: %#x\n\n", (unsigned long long)newColl->parent);
+
+
+
+
 }
--- server/CSDB/CSDB.h
+++ server/CSDB/CSDB.h
@@ -1,3 +1,5 @@
+#pragma once
+
 /**
  * Author: Ryan Steinwert
  * 
@@ -10,28 +12,60 @@ enum PERM {
     PUBLIC
 };
 
-typedef struct collection_t {
-    char* name;
-    char* path;
-    collection_t* subCollections;
-} collection_s;
+enum DTYPE {
+    TEXT,
+    IMAGE,
+    AUDIO,
+    VIDEO,
+    STREAM,
+    AUDIO_STREAM
+};
+
+union ItemData {
+    char* text;
+    char* location;
+};
 
 typedef struct item_t {
     char* name;
     char* owner;
     PERM perms;
+    DTYPE type;
+    bool loaded;
+    union ItemData data;
 } item_s;
 
+typedef struct collection_t {
+    int numSubColls;
+    char* name;
+    char* path;
+    item_s** items;
+    collection_t** subCollections;
+    collection_t* parent;
+} collection_s;
+
+
+/**
+ * Main database type, provides top level DB control
+ */
 class CSDB {
 public:
     CSDB();
     CSDB(const char* dirname);
     ~CSDB();
 
-    void loadDB(const char* filename);
 
 private:
+    // vars for parsing collections file
+    int _currentCollDepth, _collIndex;
+    collection_s* _currentParent;
+
     const char* _dbDirname;
 
+    collection_s** _collections;
+
     void setup();
+    void loadDB(const char* collsFilename);
+    
+    void parseCollectionString(char* collectionString);
 };
