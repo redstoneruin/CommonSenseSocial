2f543ff401553e7868487b904ca7837d96e92659
Ryan Steinwert <rsteinwe@ucsc.edu>
2020-12-22 12:08:18 -0800

    DB filesystem setup

--- server/CSDB/CSDB.cpp
+++ server/CSDB/CSDB.cpp
@@ -4,15 +4,35 @@
  * CSDB class implementation file
  */
 
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #include "CSDB.h"
 
 
 /**
  * CSDB constructor
  */
-CSDB::CSDB()
+CSDB::CSDB() : CSDB("db")
 {
+}
 
+/**
+ * Create db with the given filename
+ * @dirname the directory containing the database
+ */
+CSDB::CSDB(const char* dirname)
+{
+    
+    _dbDirname = dirname;
+
+    setup();
 }
 
 /**
@@ -20,4 +40,24 @@ CSDB::CSDB()
  */
 CSDB::~CSDB()
 {
+}
+
+
+/**
+ * DB setup function, loads structure
+ */
+void CSDB::setup()
+{
+    mkdir(_dbDirname, S_IRWXU);
+
+    std::string collFilename(_dbDirname);
+    collFilename.append("/collections");
+
+    // create collection file if dne
+    int collfd = open(collFilename.c_str(), O_RDWR | O_CREAT);
+    if(collfd == -1) {
+        fprintf(stderr, "Could not open collections file\n");
+    }
+
+    close(collfd);
 }
--- server/CSDB/CSDB.h
+++ server/CSDB/CSDB.h
@@ -4,8 +4,34 @@
  * Common Sense Database class definition
  */
 
+enum PERM {
+    PRIVATE,
+    UNLISTED,
+    PUBLIC
+};
+
+typedef struct collection_t {
+    char* name;
+    char* path;
+    collection_t* subCollections;
+} collection_s;
+
+typedef struct item_t {
+    char* name;
+    char* owner;
+    PERM perms;
+} item_s;
+
 class CSDB {
 public:
     CSDB();
+    CSDB(const char* dirname);
     ~CSDB();
+
+    void loadDB(const char* filename);
+
+private:
+    const char* _dbDirname;
+
+    void setup();
 };
